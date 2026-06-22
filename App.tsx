fff8287caa0e0338337ef1dce3eaf240a8d472c3
yasserrr2025-afk <yasserrr2025@gmail.com>
2026-06-22 08:03:13 +0300

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -343,7 +343,7 @@ const App: React.FC = () => {
         ab.some(i => matchesExamDate(i.date, date)) ||
         reports.some(i => matchesExamDate(i.date, date));
 
-      if (!hasDateData(filterDate)) {
+      if (!cfg?.active_exam_date && !hasDateData(filterDate)) {
         const fallbackDates = sortDateKeysDesc([
           ...sv.filter(i => !isReserveSupervision(i)).map(i => getRiyadhDateKeyFromValue(i.date)),
           ...exams.map(i => String(i.exam_date || '').slice(0, 10)),
